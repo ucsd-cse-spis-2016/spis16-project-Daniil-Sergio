7023bced8606535a2998bf884052685693a2a4c1
dkubatko <dkubatko@ucsd.edu>
2016-08-23 14:31:40 -0700

    Add files via upload

--- run_survey.py
+++ run_survey.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, url_for, render_template, request
-import time
+import glob, os
 
 
 
@@ -19,31 +19,51 @@ def render_survey():
 
 
 def render_result():
-    results = request.args
+    req = request.args
+    results = {}
+    for elem in req:
+        results[elem] = req[elem].lower()
+    
     print(results)
     print check(results)[0]
     if check(results)[0]:
         save_res(results)     
-        return render_template('success.html', name = results['first_name'])
+        return render_template('success.html', name = req['first_name'])
     else:
         return render_template('error.html', error = check(results)[1])
 
 def check(arr):
+    #print(os.getcwd())
+    if in_base(arr['nickname']):
+        print('here')
+        return False, 'username already exists'
+
+    if arr['nickname'] == '':
+            return False, 'required fields should be filled'
     for elem in arr:
         if elem != 'age' and elem != 'nickname':
-            if not(arr[elem].isalpha()):
+            if not(arr[elem].isalpha()) and arr[elem] != '':
                 return False, 'numbers in a text form'
-        if arr[elem] == 'required' or arr[elem] == 'optional':
-            return False, 'required or optional should be deleted'
-        
+        if elem == 'age' and not(arr[elem].isdigit()):
+            return False, 'letters in a digit form'
+                
     return True, 'none'
 
 def save_res(results):
-    name = results['nickname'] + time.strftime("%d_%m_%Y") + '.txt'
-    f = open(name, 'w')
+    name = results['nickname'] + '.txt'
+    f = open('users/' + name, 'w')
     for elem in results:
-        f.write(str(elem) + ' ' + str(results[elem]) + '\n')
+       f.write(str(elem) + ' ' + str(results[elem]) + '\n')
+    return True
 
+def in_base(name):
+    for file in glob.glob("users/*.txt"):
+        if str(file.split('.')[0].split('/')[1]) == name:
+            print('here')
+            return True
+    return False
+
+    
 
 @app.route('/gettop5')
 def render_gettop5():
@@ -52,7 +72,10 @@ def render_gettop5():
 
 
     
-#@app.route('/forgot')
+@app.route('/forgot')
+def forgot_nickname():
+    return render_template('forgot.html')
+    
 
 if __name__=="__main__":
     app.run(debug=False,host="128.54.69.248", port=54321)
