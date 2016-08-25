ea75a5e2792354f87e650fdbbea0f3482db10d37
Sergio <svillazo@ucsd.edu>
2016-08-25 14:13:57 -0700

    Works

--- visual_function.py
+++ visual_function.py
@@ -1,53 +1,39 @@
 "drawing the percentage match visual"
 from PIL import Image
 
-def percentvisual(percent,width,height):
-    im = Image.new("RGB", (width,height))
+def percentvisual(percent,width,height, colors):
+    im = Image.new("RGB", (width,height), (250,250,250))
+
     
     limit = (width) * percent
     count = 0
-    incrR = int (120 / (percent* width))
-    incrG = int (100 / (percent * width))
-    incrB = int (250 / (percent * width))
-    r = 0
-    g = 0
-    b = 0
-
-    print limit
-    
-    for x in range(width):
-        for y in range(height):
+    incrR = colors[0]/(percent* width)
+    incrG = colors[1]/(percent * width)
+    incrB = colors[2]/(percent * width)
+     
+    for x in range(height):
+        r = colors[0] - (percent* width)
+        if r < 0:
+            r = int(colors[0]*0.2)
+        g = colors[1] - (percent* width)
+        if g < 0:
+            g = int(colors[1]*0.2)
+        b = colors[2] - (percent* width)
+        if b < 0:
+            b = int(colors[2]*0.2)
+        count=0
+        for y in range(width):
             if count <= limit:
-                im.putpixel((x,y), (r,g,b))
+                im.putpixel((y, x), (int(r),int(g),int(b)))
                 if r <= 120:
                     r = r + incrR
-                    print r
                 if g <=100:
                     g = g + incrG
-                    print g
                 if b <= 250:
                     b = b + incrB
-                    print b 
                 count = count + 1
-                '''
-
-    print incrR
-    print incrG
-    print incrB
-    print r
-    print g
-    print b
-              
-'''
     im.show()
 
-percentvisual(.50,500,350)
+percentvisual(0.75,600,50, (10, 255, 100))
 
-'''
-  
-130 130 130
 
-0 -> 130
-130 / perscent*widgth = colored pixel
-
-'''
